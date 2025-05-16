f6e02696c0ca332a4ceee8a803035fa1e594a390
Josanva777 <johansanchezvalencia@gmail.com>
2025-05-16 10:51:08 -0500

    solucion de cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import registroRoutes from "./routes/registroRoutes.js";
 
@@ -10,25 +11,19 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["https://merkahorro.com", "http://localhost:3000"], // Ajusta según los dominios del frontend
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     optionsSuccessStatus: 200,
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.options('*', cors()); // Responder preflight
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Rutas
 app.use(registroRoutes);
-
-// Manejo de rutas no encontradas
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Ruta no encontrada",
-  });
-});
 
 // Exportar para Vercel
 export default app;
