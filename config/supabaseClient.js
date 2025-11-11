825e3b8f9c964ce06b3db2840cc17f03182c6f4f
desarrollador software <desarrollo@merkahorrosas.com>
2025-11-11 16:22:33 -0500

    se creea logica para la DB

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -5,7 +5,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseServiceKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseServiceKey);
+
+// Usamos una variable de entorno para la clave service_role
+// Su valor debe ser la clave 'service_role | secret' de tu imagen.
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY; 
+
+// Creamos el cliente con la clave de Rol de Servicio.
+// ESTE CLIENTE ACTÚA COMO ADMINISTRADOR y IGNORA EL RLS.
+const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
 
 export default supabase;
